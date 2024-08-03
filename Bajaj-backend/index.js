98e4685fdf8f1ce1d234d8f6765047070aa4baf2
shanmukhsworkspace-admin <shanmukhasai_jogi@srmap.edu.in>
2024-08-04 00:25:47 +0530

    port changes

--- Bajaj-backend/index.js
+++ Bajaj-backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Helper function to separate numbers and alphabets
 const separateData = (data) => {
